1b09dd4b63514ff0e58360d04d92f3f2cc1af467
Pablo Daniel Villagrán Casasola <pdvillc@gmail.com>
2018-05-08 00:26:09 -0600

    Agregados Manager. parte 2/2

---
+++ GestionParqueo/GestionParqueo/BDManager/VehiculoClienteManager.cs
@@ -0,0 +1,32 @@
+﻿using GestionParqueo.BDEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionParqueo.BDManager
+{
+    class VehiculoClienteManager : AbstractManager<VehiculoCliente>
+    {
+        public override void Delete(VehiculoCliente o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Insert(VehiculoCliente o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<VehiculoCliente> Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(VehiculoCliente o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
